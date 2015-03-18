dfd0b984539503307f8753e19f74666d5f51a4d9
sschwarzbard <stuart@schwarzbard.com>
2015-03-18 17:15:59 -0400

    Update problem0.py

--- dataScience/lesson7/problem0.py
+++ dataScience/lesson7/problem0.py
@@ -1,9 +1,12 @@
-__author__ = 'Me'
+# The goal is to create a function that removes punctuation from a string
 
 sentence = raw_input('Enter a sentence please: ')
 def unpunctuate(sentence):
+# For each character in string
     for char in sentence:
+# if it matches any of those in the string
         if char in "?.',!/;:":
+# replace it with a space. sentence.replace takes (the character, the replacement)
             sentence = sentence.replace(char, '')
     print sentence
 
